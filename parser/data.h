a55c84e10a3a73f6696af2321a004ee13ecfc595
DemetryF <djimabebeshko@gmail.com>
2021-02-20 18:34:53 +0300

    parser: add divider pos to Node struct

--- parser/data.h
+++ parser/data.h
@@ -9,16 +9,18 @@ struct Node
     Lexem_s **lexems;
     Node_s *left;
     Node_s *right;
-    char Operator; 
+    char Operator;
     size_t length;
+    size_t divider_pos;
 };
 
-Node_s *new_node(Lexem_s **lexems, size_t length, char Operator, Node_s *left, Node_s *right)
+Node_s *new_node(Lexem_s **lexems, size_t length, size_t divider_pos, char Operator, Node_s *left, Node_s *right)
 {
     Node_s *node = (Node_s *)malloc(sizeof(Node_s));
     
     node->lexems = lexems;
     node->length = length;
+    node->divider_pos = divider_pos;
     node->Operator = Operator;
     node->left = left;
     node->right = right;
