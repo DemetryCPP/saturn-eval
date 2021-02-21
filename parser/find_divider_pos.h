7a4907a84ea4311cb1915c0675163328e8a25eb9
DemetryF <djimabebeshko@gmail.com>
2021-02-21 10:51:59 +0300

    fix brackets in find_divider_pos.h

--- parser/find_divider_pos.h
+++ parser/find_divider_pos.h
@@ -8,15 +8,29 @@
 size_t find_divider_pos(Lexem_s **lexems, size_t length)
 {
     size_t position = -1;
+    size_t brackets = 0;
 
     for (int i = 0; i < length; i++)
     {
         Lexem_s *current = lexems[i];
 
+        if (current->Type == 4) {
+            if (current->Data == '(') brackets++;
+            else if (current->Data == ')') brackets--;
+            continue;
+        }
+
+        if (brackets != 0) continue;
         if (current->Type != 3) continue;
         
         if (current->Data == '+' || current->Data == '-') return i;
         else if (current->Data == '/' || current->Data == '*') position = i;
+    }
+
+    if (brackets != 0) 
+    {
+        printf("bracket error!\n");
+        exit(1);
     }
 
     return position;
