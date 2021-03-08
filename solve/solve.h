a2afca335297fc2856c6871550a725e1a669bfed
DemetryF <djimabebeshko@gmail.com>
2021-03-08 15:29:25 +0300

    rewrite solve.h

--- solve/solve.h
+++ solve/solve.h
@@ -34,13 +34,12 @@ double get_value(char *text)
         }
     }
     else result = atof(text);
-
     return result;
 }
 
 double solve(Node_s *node)
 {
-    if (node->left == NULL && node->right == NULL) 
+    if (node->right == NULL && node->left == NULL)
     {
         char *text = lexems_to_text(node->lexems, node->length);
         double result;
@@ -49,62 +48,20 @@ double solve(Node_s *node)
 
         free(text);
         return result;
-    } 
-    else if (node->left->left == NULL 
-     && node->left->right == NULL 
-     && node->right->left == NULL
-     && node->right->right == NULL)
-    {
-        char *leftT = lexems_to_text(node->left->lexems, node->left->length);
-        char *rightT = lexems_to_text(node->right->lexems, node->right->length);
-
-        double leftN;
-        double rightN;
-
-        leftN = get_value(leftT);
-        rightN = get_value(rightT);
-
-        if (leftN == big_num || rightN == big_num) return big_num;
-
-        free(leftT);
-        free(rightT);
-
-        return mini(leftN, rightN, node->Operator);
     }
-    else if (node->left->left == NULL
-          && node->left->right == NULL)
-    {
-        char *text = lexems_to_text(node->left->lexems, node->left->length);
-        double n = get_value(text);
 
-        if (n == big_num) return big_num;
-        
-        free(text);
+    char *leftT = lexems_to_text(node->left->lexems, node->left->length),
+         *rightT = lexems_to_text(node->right->lexems, node->right->length);
 
-        return mini(n, solve(node->right), node->Operator);
-    }
-    else if (node->right->left == NULL
-          && node->right->right == NULL)
-    {
-        char *text = lexems_to_text(node->right->lexems, node->right->length);
-        double n = get_value(text);
+    double left, right;
 
-        if (n == big_num) return big_num;
+    if (node->left->left == NULL && node->left->right == NULL) left = get_value(leftT);
+    else left = solve(node->left);
 
-        free(text);
+    if (node->right->left == NULL && node->right->right == NULL) right = get_value(rightT);
+    else right = solve(node->right);
 
-        double left = solve(node->left);
-        if (left == big_num) return big_num;
+    if (right == big_num || left == big_num) return big_num;
 
-        return mini(left, n, node->Operator);
-    }
-    else
-    {
-        double left = solve(node->left);
-        double right = solve(node->right);
-
-        if (left == big_num || right == big_num) return big_num;
-
-        return mini(left, right, node->Operator);
-    }
+    return mini(left, right, node->Operator);
 }
