e638d3fe1d478733c3380fe66d7a169e8241da55
DemetryF <djimabebeshko@gmail.com>
2021-03-08 12:49:41 +0300

    fix wrong order of execution

--- parser/find_divider_pos.h
+++ parser/find_divider_pos.h
@@ -9,7 +9,7 @@ size_t find_divider_pos(Lexem_s **lexems, size_t length)
 {
     size_t position = -1, brackets = 0, priority = 3;
 
-    for (int i = 0; i < length; i++)
+    for (int i = length - 1; i >= 0; i--)
     {
         Lexem_s *current = lexems[i];
 
