bc2517e17a28b6481d174c19921e1f3f4a5755bf
DemetryF <djimabebeshko@gmail.com>
2021-03-28 18:48:42 +0300

    functions pack

--- eval/function.h
+++ eval/function.h
@@ -7,4 +7,16 @@ double function(char *fname, double arg)
 {
     if (isf("sin")) return sin(arg);
     if (isf("cos")) return cos(arg);
+    if (isf("abs")) return fabs(arg);
+    if (isf("acos")) return acos(arg);
+    if (isf("asin")) return asin(arg);
+    if (isf("atan")) return atan(arg);
+    if (isf("ch") || isf("cosh")) return cosh(arg);
+    if (isf("sh") || isf("sinh")) return sinh(arg);
+    if (isf("exp")) return exp(arg);
+    if (isf("ln")) return log(arg);
+    if (isf("lg")) return log10(arg);
+    if (isf("sqrt")) return sqrt(arg);
+    if (isf("tan")) return tan(arg);
+    if (isf("tanh")) return tanh(arg);
 }
