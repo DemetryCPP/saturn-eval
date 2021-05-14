8dfd7434abf99c43ebbf1d73cde03c5cc9c20b61
DemetryF <djimabebeshko@gmail.com>
2021-05-14 15:19:28 +0300

    rewrite compile script

--- compile.sh
+++ compile.sh
@@ -1,1 +1,17 @@
-gcc eval/lexer/utils.c eval/lexer/lexer.c eval/parser/find_divider_pos.c eval/parser/remove_brackets.c eval/parser/utils.c eval/parser/parser.c eval/solve/constants.c eval/solve/functions.c eval/solve/operators.c eval/solve/solve.c eval/free.c eval/eval.c test_program/main.c -lm -o calc
+CC="gcc"
+FLAGS="-lm -o calc -Ieval/headers/"
+FILES="eval/lexer/utils.c 
+       eval/lexer/lexer.c 
+       eval/parser/find_divider_pos.c 
+       eval/parser/remove_brackets.c 
+       eval/parser/utils.c 
+       eval/parser/parser.c 
+       eval/solve/constants.c 
+       eval/solve/functions.c 
+       eval/solve/operators.c 
+       eval/solve/solve.c 
+       eval/free.c 
+       eval/eval.c 
+       test_program/main.c"
+
+$CC $FILES $FLAGS
