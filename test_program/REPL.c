ceabff2745e4aebad3cdde261f642828341c11f7
DemetryF <djimabebeshko@gmail.com>
2021-05-15 22:05:08 +0300

    split main.c and repl

---
+++ test_program/REPL.c
@@ -0,0 +1,84 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "eval.h"
+
+#define BUFF_SIZE 100
+
+void REPL(Constant_s **constants)
+{
+    char *input = malloc(BUFF_SIZE * sizeof(char));
+    Status_s *status = malloc(sizeof(Status_s));
+    while (1)
+    {
+        memset(input, 0, BUFF_SIZE);
+        memset(status, 0, sizeof(Status_s));
+
+        printf("> ");
+        fgets(input, BUFF_SIZE, stdin);
+
+        if (strcmp(input, "\n") == 0)
+            continue;
+        
+        if (input[0] == '.')
+        {
+            if (strcmp(input, ".exit\n") == 0) break;
+            else if (strcmp(input, ".help") == 0) 
+                printf("Git repository: https://github.com/DemetryF/Saturn-Eval\n\n7 operators: +, -, /, *, %%, \\, ^.\nBrackets: \"()\"\nDouble numbers: 2.1\nFunctions: sin(12). use .funcs in REPL to check functions list.\n");
+            else if (strcmp(input, "funcs")) 
+            {
+                FILE *fptr = fopen("eval/functions_list.txt", "r");
+                char *text = (char *)calloc(400, sizeof(char));
+
+                if (!fptr || !text)
+                {
+                    printf("an error occurred while opening the file\n");
+                    exit(1);
+                }
+
+                fread(text, 400, 200, fptr);
+                fclose(fptr);
+                puts(text);
+                free(text);
+            }
+            else printf("\"%s\" is not REPL command.\n", input);
+
+            continue;
+        }
+
+        double result = eval(input, status, constants);
+
+        switch (status->code)
+        {
+        case sc_unexped_token:
+            printf("Unexped token '%c' at %ld\n", status->data3, status->data1 + 1);
+            break;
+        
+        case sc_brackets_error:
+            printf("Brackets error\n");
+            break;
+
+        case sc_is_not_defined:
+            printf("'%s' is not defined\n", status->data2);
+            break;
+
+        case sc_is_not_a_function:
+            printf("'%s' is not a function\n", status->data2);
+            break;
+
+        case sc_unexped_end_of_line:
+            printf("Unexped end of input\n");
+            break;
+
+        case sc_zero_tokens:
+            continue;
+
+        case sc_ok:
+            printf("%g\n", result);
+            break;
+        }
+    }
+
+    free(input);
+    free(status);
+}
--- test_program/main.c
+++ test_program/main.c
@@ -1,144 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <string.h>
 #define __USE_MISC 1
-#include <math.h>
 #include "eval.h"
 
-#define BUFF_SIZE 100
+void REPL(Constant_s **constants);
 
 int main(int argc, char *argv[])
 {
-    printf("Welcome to REPL for Evaluate Mathematic Expression v%s\nType \".exit\" to exit, \".help\" to more information.\n", VERSION);
+    printf("Welcome to REPL for Saturn Eval v%s\nType \".exit\" to exit, \".help\" to more information\n", VERSION);
 
-    char *input = (char *)calloc(BUFF_SIZE, sizeof(char));
-    Constant_s **constants = (Constant_s **)malloc((argc + 2) * sizeof(Constant_s *));
-    Status_s *status = (Status_s *)malloc(sizeof(Status_s));
-    size_t constants_count = 0;
+    Constant_s **constants = malloc((argc + 2) * sizeof(Constant_s));
+    size_t constants_count = 3;
 
-    if (!constants || !input || !status)
+    if (!constants)
     {
         puts("memory allocation error.");
         exit(1);
-    }
+    }   
 
-    constants[constants_count++] = new_constant("e", M_E);
-    constants[constants_count++] = new_constant("pi", M_PI);
+    constants[0] = new_constant("e", M_E);
+    constants[1] = new_constant("pi", M_PI);
 
     for (size_t i = 1; i < argc; i++)
     {
         char *current = argv[i];
-
-        if (current[0] == '-') 
+        if (current[0] != '-') continue;
+        switch (current[1])
         {
-            switch (current[1])
+        case 'd':
+        {
+            char *copy = malloc(strlen(current + 2) * sizeof(char)), 
+                 *name;
+            if (!copy)
             {
-            case 'd':
+                puts("memory allocation error.");
+                exit(1);
+            }
+
+            strcpy(copy, current + 2);
+            name = strtok(copy, "=");
+
+            if (!name)
             {
-                char *copy = (char *)malloc(strlen(argv[i] + 2) * sizeof(char)), *name;
-
-                if (!copy)
-                {
-                    puts("memory allocation error.");
-                    exit(1);
-                }
-
-                strcpy(copy, argv[i] + 2);
-                name = strtok(copy, "=");
-
-                if (!name)
-                {
-                    puts("invalid -d flag use.");
-                    exit(1);
-                }
-
-                double value = atof(strtok(NULL, "="));
-
-                constants[constants_count] = new_constant(name, value);
-                constants[constants_count]->name = name;
-                constants[constants_count]->value = value;
-
-                constants_count++;
-                break;
+                puts("invalid -d flag usage.");
+                exit(1);
             }
-            }
+
+            double value = atof(strtok(NULL, "="));
+            Constant_s *new_const = new_constant(name, value);
+            new_const->name = name;
+            new_const->value = value;
+            constants[constants_count++] = new_const;
+
+            break;
+        }
         }
     }
 
-    constants[constants_count] = new_constant("", 0);
-
-    while (1)
-    {
-        memset(input, 0, BUFF_SIZE);
-        memset(status, 0, sizeof(Status_s));
-        status->code = sc_ok;
-
-        printf("> ");
-        fgets(input, BUFF_SIZE, stdin);     
-
-        if (strcmp(input, "\n") == 0)
-            continue;
-
-        if (input[0] == '.')
-        {
-            input[strlen(input) - 1] = '\0';
-
-            if (strcmp(input + 1, "exit") == 0) break;
-            else if (strcmp(input + 1, "help") == 0) printf("Git repository: https://github.com/DemetryF/evaluate-mathematic-expressions\n\n7 operators: +, -, /, *, %%, \\, ^.\nBrackets: \"()\"\nDouble numbers: 2.1\nFunctions: sin(12). use .funcs in REPL to check functions list.\n");
-            else if (strcmp(input + 1, "funcs") == 0)
-            {
-                FILE *fptr = fopen("eval/functions_list.txt", "r");
-                char *text = (char *)calloc(400, sizeof(char));
-
-                if (!fptr || !text)
-                {
-                    printf("an error occurred while opening the file\n");
-                    exit(1);
-                }
-
-                fread(text, 400, 200, fptr);
-                fclose(fptr);
-                printf("%s", text);
-                free(text);
-            }
-            else printf("'%s' is not REPL command\n", input + 1);
-
-            continue;
-        }
-
-        double result = eval(input, status, constants);
-
-        switch (status->code)
-        {
-        case sc_unexped_token:
-            printf("Unexped token '%c' at %ld\n", status->data3, status->data1 + 1);
-            break;
-        
-        case sc_brackets_error:
-            printf("Brackets error\n");
-            break;
-
-        case sc_is_not_defined:
-            printf("'%s' is not defined\n", status->data2);
-            break;
-
-        case sc_is_not_a_function:
-            printf("'%s' is not a function\n", status->data2);
-            break;
-
-        case sc_unexped_end_of_line:
-            printf("Unexped end of input\n");
-            break;
-
-        case sc_zero_tokens:
-            continue;
-
-        case sc_ok:
-            printf("%g\n", result);
-            break;
-        }
-    }
-
-    free(input);
-    return 0;
+    REPL(constants);
 }
